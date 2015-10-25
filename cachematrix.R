a824082720e44405aac8a3b57535b23479c58bf4
cloony <cloony@cloosigns.sk>
2015-10-25 16:11:36 +0100

    Solution Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## By Patrik Hlavac
+## Two functions based on assignment example of vector caching
+## first function defines new enhanced matrix
+## second function check if inversion was done previously and return inv.
 
-## Write a short comment describing this function
+
+## initialize new matrix with enhanced options
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setInv <- function(solve) inv <<- solve	
+	getInv <- function() inv
+	list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## check makeCacheMatrix data type value and solve matrix
+## depending on previously done calculation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inv <- x$getInv()
+	if(!is.null(inv)) {
+		message("data cache is available")
+		return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data)
+	x$setInv(inv)
+	inv
 }
+
+## Example
+## A <- matrix(c(1,1,3,2,5,6,7,4,9),3,3)
+## a <- makeCacheMatrix(A)
+## A
+## cacheSolve(a)
+## cacheSolve(a)
+
+
